5624f50281e1cae6b13de9bac7cec3696d5d24eb
MELISSAHZULU <bsc-04-22@unima.ac.mw>
2026-03-24 16:04:56 +0300

    merged branches

--- FoodStore/orders/admin.py
+++ FoodStore/orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Cart, CartItem
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'customer', 'restaurant', 'status', 'total_price', 'created']
+    list_filter   = ['status']
+    search_fields = ['customer__username', 'restaurant__name']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display  = ['order', 'menu_item', 'quantity', 'price']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display  = ['customer', 'restaurant', 'updated']
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display  = ['cart', 'menu_item', 'quantity']
