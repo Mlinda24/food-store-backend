c6a0a71b6ed130dc2a52b79b36b2e1a1d7c5db29
MLINDA MBALE <bsc_com_31_21@unima.ac.mw>
2026-04-29 17:44:48 -0800

    customised admin for restaurant app

--- FoodStore/restaurants/admin.py
+++ FoodStore/restaurants/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Restaurant, Category, MenuItem
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'owner', 'phone', 'is_open', 'created']
+    list_filter   = ['is_open']
+    search_fields = ['name', 'address']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'restaurant']
+    search_fields = ['name']
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'restaurant', 'category', 'price', 'is_available']
+    list_filter   = ['is_available', 'restaurant']
+    search_fields = ['name']
