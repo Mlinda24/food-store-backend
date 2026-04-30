936845a72277ce9302af5e505ceaf9438fa1b92f
MELISSAHZULU <bsc-04-22@unima.ac.mw>
2026-04-29 18:03:16 -0800

    changed urls

--- FoodStore/FoodStore/urls.py
+++ FoodStore/FoodStore/urls.py
@@ -1,25 +1,12 @@
-"""
-URL configuration for FoodStore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Swagger schema setup
 schema_view = get_schema_view(
     openapi.Info(
         title="Food Store API",
@@ -33,11 +20,26 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
-    path('api/orders/',      include('orders.urls')),      
+
+    # Auth endpoints
+    path('api/auth/', include('accounts.urls')),
+
+    # App endpoints
+    path('api/', include('restaurants.urls')),
+    path('api/orders/', include('orders.urls')),
+
+    # DRF login
+    path('api-auth/', include('rest_framework.urls')),
+
+    # Swagger UI
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# Serve media and static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
